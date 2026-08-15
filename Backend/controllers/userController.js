b42be252b836b32d4362d12337b9f3705aaf21bf
Kushal Pradhan <kushalpradhan127@gmail.com>
2026-08-15 22:41:25 +0530

    reverify api and login api implemented

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -7,14 +7,14 @@ const register = async (req, res) => {
   try {
     const { firstName, lastName, email, password } = req.body;
     if (!firstName || !lastName || !email || !password) {
-      res.status(400).json({
+      return res.status(400).json({
         success: false,
         message: "All field required",
       });
     }
     const user = await User.findOne({ email });
     if (user) {
-      res.status(400).json({
+      return res.status(400).json({
         success: false,
         message: "user allready exists",
       });
@@ -47,6 +47,142 @@ const register = async (req, res) => {
   }
 };
 
+const verify = async (req, res) => {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(400).json({
+        success: false,
+        message: "Authorization token is missing",
+      });
+    }
+    const token = authHeader.split(" ")[1];
+    let decoded;
+    try {
+      ((decoded = jwt.verify(token, process.env.JWT_SECRET)),
+        { expiresIn: "1m" });
+    } catch (error) {
+      if (error.name === "TokenExpiredError") {
+        return res.status(400).json({
+          success: false,
+          message: "The registration token is expired",
+        });
+      }
+      return res.status(400).json({
+        success: false,
+        message: "Token verification failed",
+      });
+    }
+    const user = await User.findById(decoded.id);
+    if (!user) {
+      return res.status(400).json({
+        success: true,
+        message: "User not found",
+      });
+    }
+    user.token = null;
+    user.isverified = true;
+    await user.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Email verified successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+const reVerify = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(200).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "10m",
+    });
+    verifyEmail(token, email);
+    user.token = token;
+    await user.save();
+    return res.status(200).json({
+      success: true,
+      message: "Verification email sent successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields required",
+      });
+    }
+
+    const exisitingUser = await User.findOne({ email });
+    if (!exisitingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      exisitingUser.password,
+    );
+    if (!isPasswordValid) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid Crediential",
+      });
+    }
+
+    if (exisitingUser.isverified === false) {
+      return res.status(400).json({
+        success: false,
+        message: "Email not verified, please verify and login",
+      });
+    }
+
+    const accessToken = await jwt.sign(
+      { id: exisitingUser._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "10d" },
+    );
+
+    const refreshToken = await jwt.sign(
+      { id: exisitingUser._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "30d" },
+    );
+
+    exisitingUser.isLoggedIn = true;
+    await exisitingUser.save();
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: "",
+    });
+  }
+};
+
 module.exports = {
   register,
+  verify,
+  reVerify,
 };
--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,8 +1,10 @@
 const exprees = require("express");
-const { register } = require("../controllers/userController");
+const { register, verify, reVerify } = require("../controllers/userController");
 
 const router = require("express").Router();
 
 router.post("/register", register);
+router.post("/verify", verify);
+router.post("/reverify", reVerify);
 
 module.exports = router;
